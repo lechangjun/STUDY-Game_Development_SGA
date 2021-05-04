7a128c788700e334633d17e25f2bf85332df4123
lechangjun <68671394+lechangjun@users.noreply.github.com>
2021-05-04 15:53:02 +0900

    Add files via upload

---
+++ game_SGA-master/Console/09_Class/main.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "Character.h"
+
+int main()
+{
+	cout << "Hello World!" << endl;
+
+	Character player;
+
+	//Character player2(100, 10,100,"sga");
+
+
+	int a = 10;
+
+	int b;
+	b = 10;
+	
+
+}
+
+
+
