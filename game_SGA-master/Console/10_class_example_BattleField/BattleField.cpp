cb004de37179428693a7166581f9e51ecf0541e1
lechangjun <68671394+lechangjun@users.noreply.github.com>
2021-05-08 02:46:03 +0900

    Add files via upload

---
+++ game_SGA-master/Console/10_class_example_BattleField/BattleField.cpp
@@ -0,0 +1,58 @@
+#include "stdafx.h"
+#include "BattleField.h"
+#include "Character.h"
+#include "Enemy.h"
+
+
+BattleField::BattleField()
+{
+	GameStart();
+}
+
+BattleField::~BattleField()
+{
+
+}
+
+void BattleField::GameStart()
+{
+	cout << "선택해" << endl;
+
+	cout << "1.상점" << endl;
+	cout << "2.필드" << endl;
+	cout << "3.병원" << endl;
+
+	cin >> select;
+
+	switch (select)
+	{
+		case 1 :
+			Store();
+			break;
+		case 2:
+			Field();
+			break;
+		case 3:
+			Hospital();
+			break;
+
+		default:
+			break;
+	}
+
+}
+
+void BattleField::Store()
+{
+	cout << "상점";
+}
+
+void BattleField::Field()
+{
+	cout << "필드";
+}
+
+void BattleField::Hospital()
+{
+	cout << "병원";
+}
---
+++ game_SGA-master/Console/10_class_example_BattleField/main.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include "BattleField.h"
+
+int main()
+{
+	BattleField field;
+
+}
+
+
+
